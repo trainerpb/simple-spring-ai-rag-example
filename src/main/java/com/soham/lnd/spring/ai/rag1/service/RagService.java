3dffd89007b3e099676e3c9976976ffd9d627cb4
SohamSirOnline <sohamsironline@gmail.com>
2025-10-16 17:19:46 +0530

    working with similar search and augmentation

--- src/main/java/com/soham/lnd/spring/ai/rag1/service/RagService.java
+++ src/main/java/com/soham/lnd/spring/ai/rag1/service/RagService.java
@@ -4,12 +4,19 @@ package com.soham.lnd.spring.ai.rag1.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.SystemPromptTemplate;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
@@ -17,6 +24,9 @@ import java.util.stream.Collectors;
 public class RagService {
     private final ChatClient chatClient;
     private final VectorStore vectorStore;
+
+    @Value("classpath:/prompt/rag-prompt.st")
+    private Resource promptResource;
 
     public RagService(ChatClient.Builder chatClientBuilder, VectorStore vectorStore) {
         this.chatClient = chatClientBuilder.build();
@@ -27,7 +37,11 @@ public class RagService {
         SearchRequest searchRequest = SearchRequest.builder().query(msg).topK(3).build();
         List<Document> similaritySearchDocuments = vectorStore.similaritySearch(searchRequest);
         String informationAsString = similaritySearchDocuments.stream().map(Document::getText).collect(Collectors.joining("\n"));
-
-        return null;
+        SystemPromptTemplate systemPromptTemplate = new SystemPromptTemplate(promptResource);
+       var prompt= new Prompt(List.of(systemPromptTemplate.createMessage(Map.of("information",informationAsString)),
+               new UserMessage(msg)
+               ));
+        var output= chatClient.prompt(prompt).call().content();
+        return output;
     }
 }
